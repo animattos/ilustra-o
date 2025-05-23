3fcb45fc83ee92b23f2a70b63fe66b692d3da841
animattos <estudioanimattos@gmail.com>
2025-05-23 01:18:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const baseIllustrations = [
     { src: './04.png', description: 'Ilustração de um super-herói preocupado em uma sala de aula com crianças entediadas.', price: 90 },
     { src: './05.png', description: 'Ilustração de um menino surpreso ou assustado andando de bicicleta.', price: 70 },
     { src: './06.png', description: 'Ilustração estilizada de um menino correndo com determinação.', price: 150 },
-    { src: '/07.png', description: 'Ilustração lúdica de um menino usando um smartphone com um dragão amigável por perto.', price: 110 },
-    { src: '/08.png', description: 'Renderização 3D estilizada de Deby Musa sorrindo.', price: 180 },
+    { src: './07.png', description: 'Ilustração lúdica de um menino usando um smartphone com um dragão amigável por perto.', price: 110 },
+    { src: './08.png', description: 'Renderização 3D estilizada de Deby Musa sorrindo.', price: 180 },
 ];
 
 const additionalModalImagesForFirstIllustration = [
